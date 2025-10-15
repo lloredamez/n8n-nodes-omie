ead3fa395b79801d457654ea510e01ffbc0138a6
rhault <raullloredagomez@gmail.com>
2025-10-15 18:00:11 -0300

    programmatic-style node

--- nodes/OmieClients/OmieClients.node.ts
+++ nodes/OmieClients/OmieClients.node.ts
@@ -1,4 +1,11 @@
-import { INodeType, INodeTypeDescription, NodeConnectionTypes } from 'n8n-workflow';
+import {
+	IDataObject,
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	NodeConnectionTypes
+} from 'n8n-workflow';
 
 export class OmieClients implements INodeType {
 	description: INodeTypeDescription = {
@@ -21,14 +28,6 @@ export class OmieClients implements INodeType {
 				required: true,
 			},
 		],
-		requestDefaults: {
-			baseURL: 'https://app.omie.com.br/api/v1/geral/clientes/',
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-		},
 		properties: [
 			{
 				displayName: 'OMIE CALL',
@@ -37,42 +36,93 @@ export class OmieClients implements INodeType {
 				options: [
 					{
 						name: 'Listar Clientes',
-						value: 'listarClientes',
+						value: 'listCustomers',
 					},
 					{
 						name: 'Consultar Cliente',
-						value: 'ConsultarCliente',
+						value: 'getCustomer',
 					},
 					{
 						name: 'Listar Clientes Resumido',
-						value: 'ListarClientesResumido',
+						value: 'listCustomersSummary',
 					},
 				],
-				default: 'ListarClientesResumido',
+				default: 'listCustomersSummary',
 				required: true,
 				noDataExpression: true,
-				routing: {
-					request: {
-						body: {
-							app_key: '={{$credentials.apiKey}}',
-							app_secret: '={{$credentials.apiSecret}}',
-							call: '={{$parameter["OMIE_CALL"]}}',
-							param: [
-								{
-									pagina: 1,
-									registros_por_pagina: 50,
-									apenas_importado_api: 'N',
-									clientesFiltro: [
-										{
-											cnpj_cpf: '',
-										},
-									],
-								},
-							],
-						},
-					},
-				},
 			},
-		],
-	};
+			{
+				displayName: 'Pagina',
+				name: 'pagina',
+				type: 'number',
+				placeholder: '10',
+				default: 1,
+				required: true,
+				description: 'Número da página a ser retornada (padrão: 1)',
+				typeOptions: {
+					minValue: 1,
+				}
+			},
+			{
+				displayName: 'Registros Por Pagina',
+				name: 'recordsPerPage',
+				type: 'number',
+				placeholder: '50',
+				default: 50,
+				required: true,
+				description: 'Número de registros por página (padrão: 50, máximo: 200)',
+				typeOptions: {
+					minValue: 1,
+				}
+			},
+		], // properties
+	}; // description
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+		const returnData: IDataObject[] = [];
+
+		for (let i: number = 0; i < items.length; i++) {
+			const page = this.getNodeParameter('pagina', i) as string;
+			const recordsPerPage = this.getNodeParameter('recordsPerPage', i) as string;
+
+			const body: IDataObject = {
+				app_key: (await this.getCredentials('omieAppApi'))!.apiKey,
+				app_secret: (await this.getCredentials('omieAppApi'))!.apiSecret,
+				call: this.getNodeParameter('OMIE_CALL', i) as string,
+				param: [
+					{
+						pagina: page,
+						registros_por_pagina: recordsPerPage,
+						apenas_importado_api: 'N',
+						clientesFiltro: [
+							{
+								cnpj_cpf: '',
+							},
+						],
+					},
+				],
+			};
+
+			const responseData = await this.helpers.httpRequest({
+				method: 'POST',
+				url: 'https://webhook.site/ca724c38-b9c9-4c35-a658-59e2341ce493',
+				body,
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			});
+
+			const results = Array.isArray(responseData) ? responseData : [responseData];
+
+			for (const result of results) {
+				returnData.push({
+					json: {
+						...result
+					}
+				});
+			}
+		}
+		return [this.helpers.returnJsonArray(returnData)];
+	}
 }
